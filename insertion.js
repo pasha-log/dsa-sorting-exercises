9375bb40f0fce156b76a782d954375baa8bfedc4
pasha-log <pasha.log@gmail.com>
2023-02-12 11:39:13 -0800

    completed first three problems

--- insertion.js
+++ insertion.js
@@ -1,3 +1,18 @@
-function insertionSort() {}
+// https://www.geeksforgeeks.org/insertion-sort/
+function insertionSort(arr) {
+	let i, key, j;
+	for (i = 1; i < arr.length; i++) {
+		key = arr[i];
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+	return arr;
+}
+// https://www.geeksforgeeks.org/insertion-sort/
 
 module.exports = insertionSort;
